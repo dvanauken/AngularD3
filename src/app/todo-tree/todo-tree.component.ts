2e54678ea1cc79c92e6872b0f1aa6db7e7be2871
Van Auken <DVANAUK@citgo.com>
2024-09-03 16:06:35 -0500

    Init dev

--- src/app/todo-tree/todo-tree.component.ts
+++ src/app/todo-tree/todo-tree.component.ts
@@ -15,54 +15,61 @@ export class TodoTreeComponent implements OnInit {
 
   constructor(private todoService: TodoService) {}
 
-  ngOnInit(): void {
-    const simpleData: Todo[] = [
-      {
-        id: 1,
-        name: "Project A",
-        description: "Main project",
-        children: [
-          {
-            id: 2,
-            name: "Module A1",
-            description: "Sub-module of Project A",
-            children: [
-              {
-                id: 4,
-                name: "Feature A1-1",
-                description: "Feature of Module A1",
-                children: []
-              }
-            ]
-          }
-        ]
-      },
-      {
-        id: 3,
-        name: "Project B",
-        description: "Secondary project",
-        children: []
-      }
-    ];
+//   ngOnInit(): void {
+//     const simpleData: Todo[] = [
+//       {
+//         id: 1,
+//         name: "Project A",
+//         description: "Main project",
+//         children: [
+//           {
+//             id: 2,
+//             name: "Module A1",
+//             description: "Sub-module of Project A",
+//             children: [
+//               {
+//                 id: 4,
+//                 name: "Feature A1-1",
+//                 description: "Feature of Module A1",
+//                 children: []
+//               }
+//             ]
+//           }
+//         ]
+//       },
+//       {
+//         id: 3,
+//         name: "Project B",
+//         description: "Secondary project",
+//         children: []
+//       }
+//     ];
+//
+//     this.dataSource.data = simpleData;
+//     console.log("Hardcoded Tree Data: ", JSON.stringify(this.dataSource.data, null, 2));
+//   }
 
-    this.dataSource.data = simpleData;
-    console.log("Hardcoded Tree Data: ", JSON.stringify(this.dataSource.data, null, 2));
+  ngOnInit(): void {
+    this.todoService.getTodos().subscribe(data => {
+      const treeData = this.buildTree(data, null);
+      this.dataSource.data = treeData;
+      console.log("Tree Data from JSON: ", JSON.stringify(treeData, null, 2));
+    });
   }
 
   hasChild = (_: number, node: Todo) => !!node.children && node.children.length > 0;
 
-  // The buildTree method can be commented out if not used
-  // private buildTree(data: Todo[], parentId: number | null): Todo[] {
-  //   return data
-  //     .filter(item => item.parent_id === parentId)
-  //     .map(item => {
-  //       const children = this.buildTree(data, item.id);
-  //       return {
-  //         ...item,
-  //         children: children.length ? children : []
-  //       };
-  //     });
-  // }
+  private buildTree(data: Todo[], parentId: number | null): Todo[] {
+    return data
+      .filter(item => item.parent_id === parentId)
+      .map(item => {
+        const children = this.buildTree(data, item.id);
+        return {
+          ...item,
+          children: children.length ? children : []
+        };
+      });
+  }
 
   toggleNode(node: Todo): void {
     console.log(`Toggling node: ${node.name}`);
